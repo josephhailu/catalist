92a07934c0dc70eca3829a199ad4f4f7848602ac
josephhailu <46454734+josephhailu@users.noreply.github.com>
2024-11-10 11:13:05 -0500

    Initial commit

---
+++ apps/client/eslint.config.js
@@ -0,0 +1,17 @@
+const nx = require('@nx/eslint-plugin');
+const baseConfig = require('../../eslint.config.js');
+const testingLibrary = require('eslint-plugin-testing-library');
+
+module.exports = [
+  ...baseConfig,
+  ...nx.configs['flat/react'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    ...testingLibrary.configs['flat/react'],
+  },
+];
---
+++ apps/client/src/app/app.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './app';
+
+describe('App', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    render(<App />);
+    expect(screen.getByText(/Welcome client/i)).toBeTruthy();
+  });
+});
