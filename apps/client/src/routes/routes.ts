c710b2c3bc0d83618f2ea88654b7efd773ddbdd7
josephhailu <46454734+josephhailu@users.noreply.github.com>
2024-11-13 23:01:36 -0500

    routes

--- apps/client/src/routes/routes.ts
+++ apps/client/src/routes/routes.ts
@@ -1,6 +1,9 @@
 export const ROUTES = {
-  FOUR0FOUR: "/*",
-  CALLBACK: "/callback",
-  INDEX: "/",
-  DASHBOARD: "/dashboard",
+  FOUR0FOUR: '/*',
+  INDEX: '/',
+  CALLBACK: '/callback',
+  COLLECTIONS: '/collections',
+  DASHBOARD: '/dashboard',
+  SETTINGS: '/settings',
+  PROFILE: '/profile',
 } as const;
